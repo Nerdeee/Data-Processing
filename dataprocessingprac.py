a5994bde82162bee3a2060deb69e72d25d996f20
Nerdeee <48033320+Nerdeee@users.noreply.github.com>
2024-06-14 15:06:45 -0700

    Update dataprocessingprac.py

--- dataprocessingprac.py
+++ dataprocessingprac.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 
-dataset = pd.read_csv('employee_data.csv')
+'''dataset = pd.read_csv('employee_data.csv')
 print(dataset.describe())
 
 # create independent and dependent variables
@@ -49,3 +49,30 @@ x_train[:,4:] = scaler.fit_transform(x_train[:, 4:])
 x_test[:,4:] = scaler.fit_transform(x_test[:, 4:])
 print(x_train)
 print(x_test)
+
+from imblearn.over_sampling import SMOTE
+from collections import Counter
+
+smote = SMOTE(random_state=27)
+smote_X_train, smote_Y_train = smote.fit_resample(x_train, y_train)
+
+print('Before SMOTE: ', Counter(y_train))
+print('After SMOTE: ', Counter(smote_Y_train))'''
+
+from sklearn.datasets import load_diabetes
+data = load_diabetes()
+
+df = pd.DataFrame(data.data, columns=data.feature_names)
+
+df['test'] = range(1, len(df)+1)
+print(df)
+
+# print(df.describe)    // shows stats of the dataset for each label such as the min, max, different quartiles, etc.
+# print(df.corr())      // shows the correlation between labels / categories.
+# print(df.nunique())   // shows the number of unique datapoints per label.
+# print(df['label_name'].unique())  // shows the number of unique datapoints for the label specified.
+# print(df.info())      // shows general information about dataframe such as the number of columns, their names, data type, non-null count, etc
+# .strip()              // string method that removes any spaces before or after non-space character string
+
+# Note: Feature selection is the process of choosing features from the original dataset whereas feature extraction is using selected features and creating new features 
+# using them, either through finding correlations betweem the selected features or some other method.
